20a845336b24708f32c8c27ee12131318d0ce4f8
foba20 <airat200mm@hotmail.com>
2024-05-21 17:27:59 +0300

    get inline button 2

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,8 @@
 import asyncio
 
-import app.keyboards as kb
+from app.keyboards import start_keyboard, kb2, items
 from aiogram import Bot, types, F, Router
 from aiogram.filters.command import Command
-from aiogram.types import FSInputFile
 
 
 router = Router()
@@ -11,7 +10,7 @@ router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message)-> None:
-    await message.reply("Привет это я ", reply_markup=kb.main)
+    await message.reply("Привет это я ", reply_markup= start_keyboard)
 
 
 @router.message(Command("hello"))
@@ -19,9 +18,26 @@ async def hello(message: types.Message)-> None:
     await message.answer(f"{message.from_user}")
 
 
+@router.message(F.text == "Начать")
+async def Go(message: types.Message)-> None:
+    await message.answer("Здарово братан !")
 
-@router.message(F.text)
-async def message(message: types.Message)-> None:
-    await message.answer("Ваше сообщение принято!")
+
+@router.message(Command("shop"))
+async def cmd_start(message: types.Message)-> None:
+    await message.reply("Привет, что хочешь купить", reply_markup=items)
 
 
+@router.callback_query(F.data == "item_laptop")
+async def device(callback: types.CallbackQuery):
+    await callback.answer("Вы выбрали ноутбук!", show_alert=True)
+
+
+@router.callback_query(F.data == "item_headphones")
+async def device(callback: types.CallbackQuery):
+    await callback.answer("Вы выбрали наушники!", show_alert=True)
+
+
+@router.callback_query(F.data == "item_phone")
+async def device(callback: types.CallbackQuery):
+    await callback.answer("Вы выбрали телефон!", show_alert=True)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,8 +1,24 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-kb =[
-    [KeyboardButton(text="Кнопка 1 ")],
-    [KeyboardButton(text="Кнопка 2 ")]
+start_keyboard =ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Начать ")],
+    [KeyboardButton(text="Закончить")]
     ]
-main = ReplyKeyboardMarkup(keyboard=kb)
+)
+
+kb2 = InlineKeyboardMarkup(
+    row_width=2,
+    inline_keyboard=[
+        [InlineKeyboardButton(text="телеграм", url="https://t.me/GPT_chat_chatgpt_bot")],
+        [InlineKeyboardButton(text="ютуб", url="youtube.com")],
+        [InlineKeyboardButton(text="GitHub", url="https://github.com/Rinirasd")]
+    ],resize_keyboard=True
+)
+items = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="телефон", callback_data="item_phone")],
+        [InlineKeyboardButton(text="ноутбук", callback_data="item_laptop")],
+        [InlineKeyboardButton(text="ВТ наушники", callback_data="item_headphones")]
+    ]
+)
